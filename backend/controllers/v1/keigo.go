3882bd9add709da29000c53cf5af06e7a76a5683
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2020-07-05 12:54:54 +0900

    :recycle: 変数の宣言でvarを省略するようにした

--- backend/controllers/v1/keigo.go
+++ backend/controllers/v1/keigo.go
@@ -17,22 +17,22 @@ const (
 func (kc *KeigoController) ConvertKeigo(c *gin.Context) {
 	kind := c.Query("kind")
 	print(kind)
-	var request models.KeigoRequest
-	var response models.KeigoResponse
+	request := models.KeigoRequest{}
+	response := models.KeigoResponse{}
 	if err := c.BindJSON(&request); err != nil {
 		c.Status(http.StatusBadRequest)
 	} else {
-		var kagome models.Kagome
+		kagome := models.Kagome{}
 		tokens := kagome.MorphologicalAnalysis(request.Body)
 		switch kind {
 		case Teinei:
-			var teinei models.Teinei
+			teinei := models.Teinei{}
 			response.ConvertedBody = teinei.Convert(tokens)
 		case Sonkei:
-			var sonkei models.Sonkei
+			sonkei := models.Sonkei{}
 			response.ConvertedBody = sonkei.Convert(tokens)
 		case Kenjyo:
-			var kenjyo models.Kenjyo
+			kenjyo := models.Kenjyo{}
 			response.ConvertedBody = kenjyo.Convert(tokens)
 		}
 		c.JSON(http.StatusOK, response)
