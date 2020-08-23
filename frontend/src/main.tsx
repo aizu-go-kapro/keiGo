f9082a68a6605f80ce906c3683ae06c81c502fd0
schktjm <si2.o.mjt@gmail.com>
2020-08-24 00:13:29 +0900

    :+1: webpackとbabelとでreactの導入

---
+++ frontend/src/main.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>Hello World</div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
