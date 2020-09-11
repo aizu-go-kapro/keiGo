2a06d1434ff05341053ad7a261d7daea73aff457
konnyaku256 <29503528+konnyaku256@users.noreply.github.com>
2020-09-11 22:50:44 +0900

    :wastebasket: originalTextの空文字チェックはKeiGoBoxコンポーネントのハンドラで通すようにしたため削除

--- frontend/src/utils/FetchAPI.tsx
+++ frontend/src/utils/FetchAPI.tsx
@@ -8,16 +8,6 @@ export const postKeiGo = async (kind: string, originalText: string): Promise<Con
   const body = {
     "body": originalText
   };
-
-  // [暫定的な対応] サーバ側実装の考慮漏れを吸収するための一時的なエラー処理
-  // 現状、Request Bodyの"body"に""を渡すとサーバ側でエラーが発生するため
-  if (originalText === "") {
-    res = {
-      converted_body: originalText
-    }
-    return res;
-  }
-
   try {
     const response = await fetch(url, {
       method: "POST",
